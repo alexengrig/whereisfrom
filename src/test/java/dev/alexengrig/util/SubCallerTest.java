7730a96b3a0c50a1198a6f1176964f396d2b2eb1
Grig Alex <alexengrigdev@gmail.com>
2022-05-28 17:05:40 +0300

    Update copyright

--- src/test/java/dev/alexengrig/util/SubCallerTest.java
+++ src/test/java/dev/alexengrig/util/SubCallerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 - 2020 Alexengrig Dev.
+ * Copyright 2020 - 2022 Alexengrig Dev.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
